b8c43a4d0591f59891fbfcbef18c4bd49ae8d8ca
Jaewoopudding <ijnn258@gmail.com>
2024-06-03 11:46:42 +0900

    actor critic training with ppo loss

--- trainer.py
+++ trainer.py
@@ -75,11 +75,11 @@ class SequenceTrainer:
             rewards,
             dones,
             rtg,
+            value,
             timesteps,
             ordering,
             padding_mask,
         ) = trajs
-        
         try:
             loss_function = losses[loss_fn]
         except:
@@ -90,6 +90,7 @@ class SequenceTrainer:
         rewards = rewards.to(self.device)
         dones = dones.to(self.device)
         rtg = rtg.to(self.device)
+        value = value.to(self.device)
         timesteps = timesteps.to(self.device)
         ordering = ordering.to(self.device)
         padding_mask = padding_mask.to(self.device)
@@ -142,62 +143,88 @@ class ValueSequenceTrainer(SequenceTrainer):
         value_optimizer,
         scheduler=None,
         device="cuda",
+        pretraining=True,
+        inner_epochs=10
     ):
-        super().__init__(model, optimizer, log_temperature_optimizer, value_optimizer, scheduler, device)
+        super().__init__(
+            model=model, 
+            optimizer=optimizer, 
+            log_temperature_optimizer=log_temperature_optimizer,
+            scheduler=scheduler, 
+            device=device,
+            pretraining=pretraining
+        )
+        
+        self.inner_epochs = inner_epochs
+        self.value_optimizer = value_optimizer
 
     def train_iteration(
         self,
         loss_fn,
         dataloader,
-    ):
-
+        finetuning_epoch=0,
+        pretraining_epoch=0,
+    ):  
         losses, nlls, entropies = [], [], []
         logs = dict()
         train_start = time.time()
-
         self.model.train()
-        for _, trajs in enumerate(dataloader):
-            loss, nll, entropy = self.train_step_stochastic(loss_fn, trajs)
+        for idx, trajs in enumerate(tqdm(dataloader, position=1, leave=False)):
+            loss, nll, entropy, value_loss = self.train_step_stochastic(loss_fn, trajs)
             losses.append(loss)
             nlls.append(nll)
             entropies.append(entropy)
-
+            wandb.log({
+                f"{self.pretraining}/train_loss": np.mean(loss),
+                f"{self.pretraining}/nll": nll,
+                f"{self.pretraining}/entropy": entropy,
+                f"{self.pretraining}/temp_value": self.model.temperature().detach().cpu().item(),
+                f"{self.pretraining}/value_loss": value_loss,
+                },
+                step=(idx + dataloader.__len__() * finetuning_epoch + pretraining_epoch),
+                commit=False
+            )
         logs["time/training"] = time.time() - train_start
-        logs["training/train_loss_mean"] = np.mean(losses)
-        logs["training/train_loss_std"] = np.std(losses)
-        logs["training/nll"] = nlls[-1]
-        logs["training/entropy"] = entropies[-1]
-        logs["training/temp_value"] = self.model.temperature().detach().cpu().item()
+        logs[f"{self.pretraining}/train_loss_mean"] = np.mean(losses)
+        logs[f"{self.pretraining}/train_loss_std"] = np.std(losses)
+        #logs["training/nll"] = nlls[-1]
+        #logs["training/entropy"] = entropies[-1]
+        #logs["training/temp_value"] = self.model.temperature().detach().cpu().item()
 
         return logs
 
-    def train_step_stochastic(self, loss_fn, value_loss_fn, trajs):
+
+    def train_step_stochastic(self, loss_fn, trajs):
         (
             states,
             actions,
             rewards,
             dones,
             rtg,
-            values, # added
+            values,
             timesteps,
             ordering,
             padding_mask,
         ) = trajs
+        
+        try:
+            loss_function = losses[loss_fn]
+        except:
+            raise ValueError(f"the loss function type {loss_fn} is unknown. There exists {losses.keys()} loss functions.")
 
-        states = states.to(self.device)
-        actions = actions.to(self.device)
-        rewards = rewards.to(self.device)
-        dones = dones.to(self.device)
-        rtg = rtg.to(self.device)
-        values = values.to(self.device)
-        timesteps = timesteps.to(self.device)
-        ordering = ordering.to(self.device)
-        padding_mask = padding_mask.to(self.device)
+        states = states.to(self.device) # (batchsize, sequence_length, state_dimension)
+        actions = actions.to(self.device) # (batchsize, sequence_length, action_dimension)
+        rewards = rewards.to(self.device) # (batchsize, sequence_length, 1)
+        dones = dones.to(self.device) # (batchsize, sequence_length)
+        rtg = rtg.to(self.device) # (batchsize, sequence_length, 1)
+        values = values.to(self.device) # (batchsize, sequence_length, 1)
+        timesteps = timesteps.to(self.device) # (batchsize, sequence_length)
+        ordering = ordering.to(self.device) # (batchsize, sequence_length)
+        padding_mask = padding_mask.to(self.device) # (batchsize, sequence_length)
 
         action_target = torch.clone(actions)
-        value_target = torch.clone(values)
 
-        _, action_preds, _, value_preds = self.model.forward(
+        state_preds, action_preds, return_preds, value_preds = self.model.forward(
             states,
             actions,
             rewards,
@@ -207,47 +234,23 @@ class ValueSequenceTrainer(SequenceTrainer):
             padding_mask=padding_mask,
         )
         
-        
-        # Loss function example -> main.py
-        # def loss_fn(
-        #     a_hat_dist,
-        #     a,
-        #     attention_mask,
-        #     entropy_reg,
-        # ):
-        #     # a_hat is a SquashedNormal Distribution
-        #     log_likelihood = a_hat_dist.log_likelihood(a)[attention_mask > 0].mean()
-
-        #     entropy = a_hat_dist.entropy().mean()
-        #     loss = -(log_likelihood + entropy_reg * entropy)
-
-        #     return (
-        #         loss,
-        #         -log_likelihood,
-        #         entropy,
-        #     )
-
-        loss, nll, entropy = loss_fn(
-            action_preds,  # a_hat_dist
-            action_target,
-            padding_mask,
-            self.model.temperature().detach(),  # no gradient taken here
+        loss, nll, entropy, value_loss = loss_function(
+            model=self.model, 
+            states=states, 
+            actions=actions,
+            rewards=rewards,
+            rtg=rtg,
+            timesteps=timesteps,
+            ordering=ordering,
+            padding_mask=padding_mask, 
+            policy_optimizer=self.optimizer,
+            value_target=values,
+            value_optimizer=self.value_optimizer,
+            inner_epochs=self.inner_epochs,
+            clip_range=0.2,
+            gamma=0.99
         )
         
-        td_loss = value _loss_fn( # add value loss function
-            value_preds,
-            value_target
-        )
-        
-        total_loss = loss + td_loss
-        
-        self.optimizer.zero_grad()
-        self.value_optimizer.zero_grad()
-        total_loss.backward()
-        torch.nn.utils.clip_grad_norm_(self.model.parameters(), 0.25)
-        self.optimizer.step()
-        self.value_optimizer.step()
-
         self.log_temperature_optimizer.zero_grad()
         temperature_loss = (
             self.model.temperature() * (entropy - self.model.target_entropy).detach()
@@ -262,4 +265,5 @@ class ValueSequenceTrainer(SequenceTrainer):
             loss.detach().cpu().item(),
             nll.detach().cpu().item(),
             entropy.detach().cpu().item(),
+            value_loss.detach().cpu().item()
         )
