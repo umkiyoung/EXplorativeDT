ed1d9dea94de9d355ae837ba8743c82a016b340b
Jaewoopudding <ijnn258@gmail.com>
2024-06-08 21:40:23 +0900

    making pretrained agent script

--- scripts/make_pretrained_agents.sh
+++ scripts/make_pretrained_agents.sh
@@ -1,11 +1,109 @@
-device=cuda:0
+device=cuda:1
 
-python ExDT.py --save_dir=pretrained_policy --off_policy_tuning --finetune_loss_fn=ODT --device=$device --env=walker2d-medium-replay-v2 --learning_rate=0.001 --num_updates_per_pretrain_iter=10000 --weight_decay=0.001 --eval_context_length=5 --eval_rtg=5000 --online_rtg=10000&
-python ExDT.py --save_dir=pretrained_policy --off_policy_tuning --finetune_loss_fn=ODT --device=$device --env=halfcheetah-medium-v2 --num_updates_per_pretrain_iter=5000 --learning_rate=0.0001 --weight_decay=0.0005 --eval_context_length=5 --eval_rtg=6000 --online_rtg=12000&
-python ExDT.py --save_dir=pretrained_policy --off_policy_tuning --finetune_loss_fn=ODT --device=$device --env=halfcheetah-medium-replay-v2 --num_updates_per_pretrain_iter=5000 --learning_rate=0.0001 --weight_decay=0.0005 --eval_context_length=5 --eval_rtg=6000 --online_rtg=12000&
+python ./ExDT.py \
+    --env=halfcheetah-medium-v2 \
+    --tag=$tag \
+    --pretrain_loss_fn=ODT \
+    --finetune_loss_fn=ODT \
+    --learning_from_offline_dataset \
+    --device=$device  \
+    --num_updates_per_pretrain_iter=5000 \
+    --learning_rate=0.0001 \
+    --weight_decay=0.0005 \
+    --eval_context_length=5 \
+    --eval_rtg=6000 \
+    --online_rtg=12000 \
+    --num_updates_per_online_iter=300 \
+    --off_policy_tuning \
+    --save_dir=pretrained_policy &
 
-wait
 
-python ExDT.py --save_dir=pretrained_policy --off_policy_tuning --finetune_loss_fn=ODT --device=$device --env=hopper-medium-v2 --num_updates_per_pretrain_iter=5000 --learning_rate=0.0001 --weight_decay=0.0005 --eval_context_length=5 --eval_rtg=3600 --online_rtg=7200 & 
-python ExDT.py --save_dir=pretrained_policy --off_policy_tuning --finetune_loss_fn=ODT --device=$device --env=hopper-medium-replay-v2 --learning_rate=0.002 --num_updates_per_pretrain_iter=5000 --weight_decay=0.0001 --eval_context_length=5 --eval_rtg=3600 --online_rtg=7200 & 
-python ExDT.py --save_dir=pretrained_policy --off_policy_tuning --finetune_loss_fn=ODT --device=$device --env=walker2d-medium-v2 --learning_rate=0.001 --num_updates_per_pretrain_iter=10000 --weight_decay=0.001 --eval_context_length=5 --eval_rtg=5000 --online_rtg=10000
+python ./ExDT.py \
+    --env=halfcheetah-medium-replay-v2 \
+    --tag=$tag \
+    --pretrain_loss_fn=ODT \
+    --finetune_loss_fn=ODT \
+    --learning_from_offline_dataset \
+    --device=$device  \
+    --num_updates_per_pretrain_iter=5000 \
+    --learning_rate=0.0001 \
+    --weight_decay=0.0005 \
+    --eval_context_length=5 \
+    --eval_rtg=6000 \
+    --online_rtg=12000 \
+    --num_updates_per_online_iter=300 \
+    --off_policy_tuning \
+    --save_dir=pretrained_policy 
+
+
+python ./ExDT.py \
+    --env=hopper-medium-v2 \
+    --tag=$tag \
+    --pretrain_loss_fn=ODT \
+    --finetune_loss_fn=ODT \
+    --learning_from_offline_dataset \
+    --device=$device  \
+    --num_updates_per_pretrain_iter=5000 \
+    --learning_rate=0.0001 \
+    --weight_decay=0.0005 \
+    --eval_context_length=5 \
+    --eval_rtg=3600 \
+    --online_rtg=7200 \
+    --num_updates_per_online_iter=300 \
+    --off_policy_tuning \
+    --save_dir=pretrained_policy &
+
+
+python ./ExDT.py \
+    --env=hopper-medium-replay-v2 \
+    --tag=$tag \
+    --pretrain_loss_fn=ODT \
+    --finetune_loss_fn=ODT \
+    --learning_from_offline_dataset \
+    --device=$device  \
+    --num_updates_per_pretrain_iter=5000 \
+    --learning_rate=0.002 \
+    --weight_decay=0.0001 \
+    --eval_context_length=5 \
+    --eval_rtg=3600 \
+    --online_rtg=7200 \
+    --num_updates_per_online_iter=300 \
+    --off_policy_tuning \
+    --save_dir=pretrained_policy 
+
+
+python ./ExDT.py \
+    --env=walker2d-medium-v2 \
+    --tag=$tag \
+    --pretrain_loss_fn=ODT \
+    --finetune_loss_fn=ODT \
+    --learning_from_offline_dataset \
+    --device=$device  \
+    --num_updates_per_pretrain_iter=10000 \
+    --learning_rate=0.001 \
+    --weight_decay=0.001 \
+    --eval_context_length=5 \
+    --eval_rtg=5000 \
+    --online_rtg=10000 \
+    --num_updates_per_online_iter=300 \
+    --off_policy_tuning \
+    --save_dir=pretrained_policy &
+
+
+python ./ExDT.py \
+    --env=walker2d-medium-replay-v2 \
+    --tag=$tag \
+    --pretrain_loss_fn=ODT \
+    --finetune_loss_fn=ODT \
+    --learning_from_offline_dataset \
+    --device=$device  \
+    --num_updates_per_pretrain_iter=10000 \
+    --learning_rate=0.001 \
+    --weight_decay=0.001 \
+    --eval_context_length=5 \
+    --eval_rtg=5000 \
+    --online_rtg=10000 \
+    --num_updates_per_online_iter=300 \
+    --off_policy_tuning \
+    --save_dir=pretrained_policy \ 
+
